d1e392a6367547c79682979a8c00f46c62bc1c82
desireebellan <56315907+desireebellan@users.noreply.github.com>
2022-03-03 12:56:40 +0100

    Delete final final directory

--- final final/static/js/keyboard3.js
+++
@@ -1,5 +0,0 @@
-function Write(letter){
-	if (letter != '-'){
-	var value = document.getElementById("uname").value
-	document.getElementById("uname").value = value.concat(letter);}
-}
--- final final/static/js/settings.js
+++
@@ -1,9 +0,0 @@
-var Color = document.forms['setting']['backSet'];
-var color = 'red'
-function changeColor(){
-		document.body.style.background = color;
-		Color.setAttribute('class','button '+color+'Pallette')
-		if (color == 'red'):
-			color = 'blue'
-				
-	}
